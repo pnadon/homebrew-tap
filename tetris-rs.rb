dc5232721c0f1e7fa682634641e9548dfd5079a2
Phil Nadon <phil@nadon.io>
2020-10-07 11:42:56 -0600

    Update tetris-rs.rb

--- tetris-rs.rb
+++ tetris-rs.rb
@@ -1,6 +1,6 @@
 class TetrisRs < Formula
   desc "A tetris game written in Rust"
-  homepage "https://github.com/k-vernooy/tetris"
+  homepage "https://github.com/pnadon/tetris-rs"
   url "https://github.com/pnadon/tetris-rs/archive/0.1.tar.gz"
   version "0.1"
   sha256 "327b2dda4d5a78cedf378df9e9bac4ce0fc3dfd705957ccfc75e2ede5fb18f58"
